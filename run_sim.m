ab9810296d5f72ef8378b4773adbd4909de80e3d
r-drummond <ross.drummond@sheffield.ac.uk>
2024-05-29 12:18:54 +0100

    Add files via upload

--- run_sim.m
+++ run_sim.m
@@ -1,7 +1,14 @@
 clc; close all; clear;
+% Code for "Mapping back and forth between model predictive control and neural
+% networks"  presented at the Learning for Dynamics and Control Conference, 2024.
+% Authors: Ross Drummond, Pablo Baldivieso, and Giorgio Valmorbida.
+
+% The code generates an implicit neural network (NN) characterisation of a model
+% predictive control (MPC) policy. This implicit NN is unravelled using the
+% method discussed in the report.
 
 
-%% MPC problem definition
+%% MPC problem setup
 n = 10; nx = 2*n;
 scaling = 1*1e0;
 
@@ -40,7 +47,7 @@ opt_details.set_solver = 'sedumi';
 b_qp = w;
 A_qp = G;
 
-%%
+%% Simulate the MPC policy. Details from paper. 
 K = 25;
 
 x0 = -[1;1]*2e2;
@@ -49,7 +56,6 @@ u = sdpvar(n,1);
 u_mpc = zeros(K,1); u_YALMIP = zeros(n,K); xk_store_MPC = zeros(N,K);
 
 for j = 1:K
-    
     f_yalmip =  xk'*2*[A'*Qtilde*B,zeros(N,n-1)];
     cost = u'*H*u + 1e0*f_yalmip*u;
     Constraints = [A_qp*u <= b_qp];
@@ -64,12 +70,15 @@ end
 xk_store_MPC;
 
 
-%%
-iters = 1e3;
+%% Simulate the implicit neural network policy.
+% The code unravels the implicit NN into an explicit one, as detailed in
+% the pape.r 
+
+iters = 1e3; % set the number of iterations of the NN unravlelling
 y_store = zeros(nx,iters); phi_store = zeros(nx,iters); res_store = zeros(nx,iters);res_norm = zeros(iters,1); sign_store = ones(nx,iters);
 
 xk = x0;
-c_MPC = 1*S*xk+ 1*w;
+c_MPC = 1*S*xk+ 1*w;% initialise the implicit NN.
 zeta = -1*c_MPC;
         
 y0 = zeros(nx,1);
@@ -79,20 +88,18 @@ for g = 1:nx
         phi(g) = y0(g);
     else
         phi(g) = 0;
-        %         phi(g) = -y(g);
     end
 end
 
 residual = y0-D*phi-zeta;
-
 K_gain = 0; y = y0;
 
 u_nn = zeros(K,1); u_ramp = zeros(n,K); xk_store_nn = zeros(N,K);
 res_norm_store = zeros(K,iters);
 for k = 1:K
-    for j = 1:iters
-        c_MPC = 1*S*xk+ 1*w;
-        zeta = -1*c_MPC;
+    for j = 1:iters % unravel the implicit NN into an explicit one
+        c_MPC = 1*S*xk+ 1*w; %define the implicit NN weights
+        zeta = -1*c_MPC; 
         y_store (:,j) = y;
         phi_store (:,j) = phi; res_store (:,j) = residual;
         ykp1 = D*phi + zeta + 1*K_gain*(residual);
@@ -103,33 +110,30 @@ for k = 1:K
                 phi(g) = ykp1(g);
             else
                 phi(g) = 0;
-                %             phi(g) = -ykp1;
                 sign_store(g,j) = -1;
             end
         end
         y  = ykp1;
-        residual = y-D*phi-zeta;
-        res_norm(j) = norm(residual);
+        residual = y-D*phi-zeta; % compute residuals
+        res_norm(j) = norm(residual); 
         res_norm_store(k,j)= norm(y-D*phi-zeta);
     end
     
     y_ramp = ykp1;
-    u_ramp(:,k) = -1*inv(H)*(F_add*xk+G'*phi);
+    u_ramp(:,k) = -1*inv(H)*(F_add*xk+G'*phi); % get the output
     
-    u_nn(k) = u_ramp(1,k);
-    xk = A*xk+B*u_nn(k);
+    u_nn(k) = u_ramp(1,k); % the step-ahead control action
+    xk = A*xk+B*u_nn(k); % implement the state update
     xk_store_nn(:,k) = xk;
 end
 
-combined = [xk_store_nn;xk_store_MPC];
+combined = [xk_store_nn;xk_store_MPC]; % compare the results
 
 
-%%
+%% Plot the results.
 my_color = [0.9,0.9,0.9];
 close all
 f_size = 18; font_size_leg = 15; f_size_leg = 15;
-
-
 
 fig1 = figure;
 plot(1:K, xk_store_nn(1,:),'-xk','color',[0.2 0.2 0.2],'linewidth',2,'markersize',12); hold on;
